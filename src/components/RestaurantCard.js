7b9bdfb02db30a48603c3008d6f6c235b5841c65
dianajyoo <diana.yooj@gmail.com>
2019-10-16 13:53:02 -0400

    separate multiple tags with commas

--- src/components/RestaurantCard.js
+++ src/components/RestaurantCard.js
@@ -11,18 +11,21 @@ const listStyle = {
 }
 
 const RestaurantCard = ({ restaurant }) => {
+  const address = restaurant.venue.location.formattedAddress.map(line => {
+    return <li>{line}</li>;
+  });
+  const tags = restaurant.venue.categories.map(category => {
+    return '#' + category.shortName.toLowerCase().replace(/\s/g, '');
+  });
+
   return (
     <div className='restaurantCard' style={divStyle}>
       <img src={'https://images.unsplash.com/photo-1497515114629-f71d768fd07c?ixlib=rb-1.2.1&auto=format&fit=crop&w=200&q=80'} alt='restaurantImage' />
 
       <ul className='restaurantDetails' style={listStyle}>
         <li>{restaurant.venue.name}</li>
-        {restaurant.venue.location.formattedAddress.map(line => {
-          return <li>{line}</li>;
-        })}
-        {restaurant.venue.categories.map(category => {
-          return <li>#{category.shortName.toLowerCase().replace(/\s/g, '')}</li>;
-        })}
+        {address}
+        <li>{tags.length ? tags.join(', ') : tags}</li>
       </ul>
     </div>
   );
