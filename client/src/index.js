c16c8fe1426b520bd68d447bc043c1235761db31
dianajyoo <diana.yooj@gmail.com>
2020-01-03 13:13:00 -0500

    add images and update styles

--- client/src/index.js
+++ client/src/index.js
@@ -1,19 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createStore, combineReducers } from 'redux';
-import { Provider } from 'react-redux'; 
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
 import './index.css';
 import App from './App';
+import reducer from './store/reducers/index';
 import * as serviceWorker from './serviceWorker';
 
-// const store = createStore();
+const store = createStore(reducer, applyMiddleware(thunk));
 
-// ReactDOM.render(<Provider>
-//   <App />
-// </Provider>, document.getElementById('root'));
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
